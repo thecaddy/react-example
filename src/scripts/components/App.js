e3fb8effd8f4b5657df9e12a32ed68e0b0cc8f5a
Jason Jung <jwkjng@msn.com>
2014-03-19 13:26:11 -0700

    after the talk

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -18,21 +18,9 @@ require('../../styles/main.css');
  * Hello World App
  */
 
-var HelloWorldApp = React.createClass({
-  render: function () {
-    return <header><h1>Hello</h1><h3>{this.props.name}</h3></header>;
-  }
-});
-
-React.renderComponent(
-  <HelloWorldApp name="Zillow" />,
-  document.getElementById('content')
-);
-
-
 // var HelloWorldApp = React.createClass({
-//   render: function() {
-//     return <div>Hello {this.props.name}!!</div>;
+//   render: function () {
+//     return <header><h1>Hello</h1><h3>{this.props.name}</h3></header>;
 //   }
 // });
 //
@@ -40,59 +28,74 @@ React.renderComponent(
 //   <HelloWorldApp name="Zillow" />,
 //   document.getElementById('content')
 // );
-
-module.exports = HelloWorldApp;
+//
+// module.exports = HelloWorldApp;
 
 
 /**
  * IDX APP
  */
 
-// var IdxApp = React.createClass({
-//
-//   getInitialState: function () {
-//     return { points: [] };
-//   },
-//
-//   handlePropertySelected: function (propertyItem) {
-//
-//     var points = [];
-//     points[0] = {
-//       latitude: propertyItem.Latitude,
-//       longitude: propertyItem.Longitude,
-//       title: propertyItem.Address
-//     };
-//
-//     this.setState({ points: points});
-//   },
-//
+var IdxApp = React.createClass({
+
+  getInitialState: function () {
+    return { points: [] };
+  },
+
+  handlePropertySelected: function (propertyItem) {
+
+    var points = [];
+    points[0] = {
+      latitude: propertyItem.Latitude,
+      longitude: propertyItem.Longitude,
+      title: propertyItem.Address
+    };
+
+    this.setState({ points: points});
+  },
+
+  render: function () {
+    var points = this.state.points;
+    return (
+      <div class="idx-results">
+        <div id="content2">
+          <PropertyList
+            source="http://192.168.62.210/results"
+            onPropertySelected={this.handlePropertySelected} />
+        </div>
+
+        <div id="content3">
+          <Map
+            latitude={33.10488}
+            longitude={-117.23401}
+            zoom={6}
+            width={400}
+            height={400}
+            points={points} />
+        </div>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(
+  <IdxApp />,
+  document.getElementById('content')
+);
+
+module.exports = IdxApp;
+
+
+
+
+// CHEATSHEET
+// var HelloWorldApp = React.createClass({
 //   render: function () {
-//     var points = this.state.points;
-//     return (
-//       <div class="idx-results">
-//         <div id="content2">
-//           <PropertyList
-//             source="http://192.168.62.212/results"
-//             onPropertySelected={this.handlePropertySelected} />
-//         </div>
-//
-//         <div id="content3">
-//           <Map
-//             latitude={33.10488}
-//             longitude={-117.23401}
-//             zoom={6}
-//             width={400}
-//             height={400}
-//             points={points} />
-//         </div>
-//       </div>
-//     );
+//     return <header><h1>Hello</h1><h3>{this.props.name}</h3></header>;
 //   }
 // });
 //
 // React.renderComponent(
-//   <IdxApp />,
+//   <HelloWorldApp name="Zillow" />,
 //   document.getElementById('content')
 // );
-//
-// module.exports = IdxApp;
